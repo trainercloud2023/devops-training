68ad5e82b4c5d0c61623a5cae4608ab4adbdb7c6
trainercloud2023 <127465924+trainercloud2023@users.noreply.github.com>
2023-03-10 09:50:29 +0530

    Create New-work.sh

---
+++ New-work.sh
@@ -0,0 +1,20 @@
+Git overview -- done
+GIT architecture -- done
+What VCS ? why vcs and types of VCS? -- done
+Git installation -- done
+Basic commands: init, add,commit,status,log,show,restore,rm -- done
+Branches & Branching Strategy -- done
+stashes --done
+merge and rebase --done
+Advanced commands: clone, push, pull --- done
+  * Clone - http clone and ssh clone -- done
+Github - Account creation
+Remotes
+Pull Request
+Github Administration: 
+ * Collabration
+ * Branch protection
+ * Git and Github integration -- done
+Scenario based questions and interview questions
+Documentation examples
+==============================================
